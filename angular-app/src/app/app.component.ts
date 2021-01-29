ff9857dc866ebc8bc98010810e00290932d1424d
Martin Grotz <martin.grotz@gmx.de>
2021-01-29 21:40:03 +0100

    prepare for building of angular app

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,32 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
+    <h1>Content Projection Demo</h1>
+    <nav>
+      <ul>
+        <li><a href=".">Angular component</a></li>
+
+        <li><a href=".">ng-content simple</a></li>
+        <li><a href=".">ng-content selectors and lifecycle</a></li>
+        <li><a href=".">ng-content multiple layers</a></li>
+        <li><a href=".">ng-content projecting twice</a></li>
+        <li><a href=".">ng-content host-context</a></li>
+        <li><a href=".">ng-content injector tree</a></li>
+
+        <li><a href=".">ng-template simple</a></li>
+        <li><a href=".">ng-template context</a></li>
+        <li><a href=".">ng-template lifecycle</a></li>
+        <li><a href=".">ng-template viewchildren/querylist</a></li>
+        <li><a href=".">ng-template multiple templates</a></li>
+        <li><a href=".">ng-template host-context</a></li>
+        <li><a href=".">ng-template injector tree</a></li>
+        <li><a href=".">ng-template performance</a></li>
+      </ul>
+    </nav>
     <router-outlet></router-outlet>
   `,
-  styles: []
+  styles: [],
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = "content-projection-demo";
 }
